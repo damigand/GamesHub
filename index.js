ece698af696a6213bdf365087503c1770c395d6e
David Miguel Andrés <damigand700@gmail.com>
2024-09-07 01:30:52 +0200

    pagina principal hecha
    Pagina principal usando el nuevo @container para responsive

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const $ = (el) => document.querySelector(el);
 
-const app = $("#app");
+const app = $('#app');
 
 app.innerHTML = `
-    <h1>What game would you like to play?</h1>
+    <h1>Choose a game</h1>
     <ul class="games">
-        <li>
-            <h3>Rock, Paper, Scissors</h3>
-            <a href="games/rps.html" class="play">PLAY</a>
-        </li>
-        <li>
-            <h3>Three in a row</h3>
-            <a href="games/threeinrow.html" class="play">PLAY</a>
-        </li>
-        <li>
-            <h3>Matching cards</h3>
-            <a href="games/cards.html" class="play">PLAY</a>
-        </li>
+        <a href="games/rps.html" class="play">
+            <li>
+                <h3>Rock, Paper, Scissors</h3>
+            </li>
+        </a>
+        <a href="games/threeinrow.html" class="play">
+            <li>
+                <h3>Three in a row</h3>
+            </li>
+        </a>
+        <a href="games/cards.html" class="play">
+            <li>
+                <h3>Matching cards</h3>
+            </li>
+        </a>
     </ul>
 `;
