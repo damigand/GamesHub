577d485fe61f5b0fda39d1f5fa84090365679e19
David Miguel Andrés <damigand700@gmail.com>
2024-09-07 18:23:40 +0200

    rock paper scissors styling

--- components/rps/rps.js
+++ components/rps/rps.js
@@ -5,30 +5,48 @@ export function rpsGame() {
         <h1>Rock, Paper, Scissors</h1>
         <div class="rps">
             <div class="score">
-                <div class="player-score">
-                    <span>Tú</span>
-                    <img src="" alt="player icon" />
+                <div class="player-icon">
+                    <span>TÚ</span>
+                    <img src="../player.png" alt="player icon" />
                 </div>
-                <div class="ia-score">
-                    <span>IA</span>
-                    <img src="" alt="ia icon" />
+                <div class="numbers">
+                    <span class="player-score">1</span>
+                    <span>-</span>
+                    <span class="ia-score">1</span>
+                </div>
+                <div class="ai-icon">
+                    <span>AI</span>
+                    <img src="../ai.png" alt="ia icon" />
                 </div>
             </div>
             <div class="game">
                 <div class="player-option">
-                    <img src="" alt="player option" />
-                    <span></span>
+                    <img src="../question.png" alt="player option" />
+                    <span>?</span>
                 </div>
+                <button class="play">PLAY</button>
                 <div class="ia-option">
-                    <img src="" alt="ia option" />
-                    <span></span>
+                    <img src="../question.png" alt="ia option" />
+                    <span>?</span>
                 </div>
             </div>
-            <div class="options">
-                <img src="../scissors.png" alt="scissors image" />
-                <img src="../rock.png" alt="rock image" />
-                <img src="../paper.png" alt="paper image" />
+            <div class="container">
+                <div class="options">
+                    <div class="scissors">
+                        <span>Scissors</span>
+                        <img src="../scissors.png" alt="scissors image" />
+                    </div>
+                    <div class="rock">
+                        <span>Rock</span>
+                        <img src="../rock.png" alt="rock image" />
+                    </div>
+                    <div class="paper">
+                        <span>Paper</span>
+                        <img src="../paper.png" alt="paper image" />
+                    </div>
+                </div>
             </div>
+            
         </div>
     `;
 }
