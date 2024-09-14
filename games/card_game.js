cdccfd26366837978af68fdd5f9cbb64b7692bf2
David Miguel Andrés <damigand700@gmail.com>
2024-09-14 16:11:11 +0200

    Card memory game start
    first few styles done, only difficulty section available for now

--- games/card_game.js
+++ games/card_game.js
@@ -1,1 +1,9 @@
-import "@c/cards/cards.js";
+import { cardGame, cardEvents } from "@c/cards/cards.js";
+import { backButton } from "@/components/backButton/back";
+
+const $ = (el) => document.querySelector(el);
+const game = $("#game");
+
+game.innerHTML += await cardGame();
+game.insertBefore(backButton(), $("h1"));
+cardEvents();
